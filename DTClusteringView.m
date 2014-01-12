38b5c0058f3877d9de4b9b25779bca6ad077b35d
Danila Parhomenko <buchanen@mail.ru>
2014-01-12 16:52:13 +0400

    Comments again

--- DTClusteringView.m
+++ DTClusteringView.m
@@ -39,6 +39,7 @@
     s.width *= 1.0/2.4; // no objc calls and stupid code for faster drawing
     s.height *= 1.0/2.4;
     CGContextSetRGBFillColor(context, 0, 0, 0, 1.0);
+    // drawing node cloud (clusterless nodes only)
     for (DTNodeX *node in self.allNodes) {
         if (node.cluster) continue;
         CGFloat x = TRANSX(node.value);
@@ -49,9 +50,8 @@
     if (!self.allClusters) return;
     CGContextSetRGBStrokeColor(context, 0.8, 0, 0.8, 1.0);
     CGContextSetLineWidth(context, 2.0);
+    // drawing node cloud (clustered nodes only)
     [self.allClusters enumerateObjectsUsingBlock:^(DTCluster *cluster, NSUInteger idx, BOOL *stop) {
-        CGFloat x = TRANSX(cluster.centerX);
-        CGFloat y = TRANSY(cluster.centerY);
         CGFloat l = cluster.length * 0.5 * s.width;
         if (l < 1.0) l = .5;
         CGContextSetRGBFillColor(context, idx & 1, (idx & 2) >> 1, (idx & 4) >> 2, 0.5);
@@ -63,10 +63,15 @@
             }
         CGContextFillPath(context);
         
-        CGContextMoveToPoint(context, x + l*cos(cluster.angle), y + l*sin(cluster.angle));
-        CGContextAddLineToPoint(context, x - l*cos(cluster.angle), y - l*sin(cluster.angle));
-        CGContextStrokePath(context);
+        // clusters' pseudolines:
+        //CGFloat x = TRANSX(cluster.centerX);
+        //CGFloat y = TRANSY(cluster.centerY);
+        //CGContextMoveToPoint(context, x + l*cos(cluster.angle), y + l*sin(cluster.angle));
+        //CGContextAddLineToPoint(context, x - l*cos(cluster.angle), y - l*sin(cluster.angle));
+        //CGContextStrokePath(context);
     }];
+    
+    // calculating overall cluser line. Not the best place for this code, actually
     NSMutableArray *line = [@[self.allClusters.firstObject] mutableCopy];
     NSMutableArray *rest = [self.allClusters mutableCopy];
     [rest removeObject:line.firstObject];
@@ -84,6 +89,7 @@
             [rest removeObject:newStart];
         }
     }
+    // drawing overall cluster line
     CGContextSetLineWidth(context, 20);
     CGContextSetRGBStrokeColor(context, 0.1, 0.1, 0.1, 0.2);
     [line enumerateObjectsUsingBlock:^(DTCluster *c, NSUInteger idx, BOOL *stop) {
